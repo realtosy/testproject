0ab0d300df67633d2bf704b85516364b2bb2e30a
realtosy <42302240+realtosy@users.noreply.github.com>
2018-11-15 14:56:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,730 @@
+    var content = {
+      Name: [
+          'Thor Walton',
+          'Quinn Flynn',
+          'Jennifer Acosta',
+          'Haley Kennedy',
+          'Brielle Williamson',
+          'Michael Silva',
+          'Bradley Greer',
+          'Dai Rios',
+          'Herrod Chandler',
+          'Zorita Serrano',
+          'Emmaline Delamora',  
+          'Sharyn Whitmire',
+          'Emilia Bowlin',
+          'Florine Pecinovsky',
+          'Cheri Wasden',
+          'Timika Viars',
+          'Virgina Lango',
+          'Rosa Tunnell',
+          'Sol Midgette',
+          'Madeleine Hanshaw',
+          'Otilia Host',
+          'Lester Milone',
+          'Shiloh Rodger',
+          'Carter Bame',
+          'Belinda Eastham',
+          'September Mcmillian',
+          'Jenette Hensley',
+          'Madlyn Merry',
+          'Renetta Ruddick',
+          'Lekisha Mcdade',
+          'Berenice Wilcoxon',
+          'Ernesto Cruikshank',
+          'Glynis Brumfield',
+          'Berry Melody',
+          'Drema Miesner',
+          'Shellie Colonna',
+          'Omer Mathew',
+          'Keith Burdette',
+          'Shanon Schurr',
+          'Silas Stull',
+          'Tyson Magnusson',
+          'Alycia Lonzo',
+          'Willie Elsworth',
+          'Sylvie Penrose',
+          'Alvaro Schaff',
+          'Eleanor Dockery',
+          'Roscoe Mangus',
+          'Lydia Rickards',
+          'Roselee Mize',
+          'Herlinda Rezendes',
+          'Bobby Beaudreau',
+          'Qiana Mahi',
+          'Maryrose Lach',
+          'Katlyn Estrella',
+          'Elvina Tootle',
+          'Adella Mangano',
+          'Tana Garlington',
+          'Reva Infante',
+          'Angelita Yancey',
+          'Mozell Vrabel'
+  ],
+      Position: [
+          'Developer',
+          'Support Lead',
+          'Junior Javascript Developer',
+          'Senior Marketing Designer',
+          'Integration Specialist',
+          'Marketing Designer',
+          'Software Engineer',
+          'Personel Lead',
+          'Sales Assisstant',
+          'Software Engineer',
+          'Occupational Therapist',
+          'Event Planner',
+          'Marketing Manager',
+          'Physicist',
+          'Veterinarian',
+          'Chemist',
+          'Social Worker',
+          'Lawyer',
+          'Medical Assistant',
+          'Financial Advisor',
+          'Patrol Officer',
+          'Referee',
+          'Electrician',
+          'Reporter',
+          'Judge',
+          'Recreational Therapist',
+          'Bookkeeping clerk',
+          'Public Relations Specialist',
+          'Statistician',
+          'Economist',
+          'Clinical Laboratory Technician',
+          'Painter',
+          'Historian',
+          'Auto Mechanic',
+          'Court Reporter',
+          'Loan Officer',
+          'College Professor',
+          'HR Specialist',
+          'Truck Driver',
+          'Interpreter & Translator',
+          'Security Guard',
+          'Farmer',
+          'Teacher Assistant',
+          'Bus Driver',
+          'Diagnostic Medical Sonographer',
+          'Professional athlete',
+          'Janitor',
+          'Personal Care Aide',
+          'Cost Estimator',
+          'Compliance Officer',
+          'Computer Programmer',
+          'Coach',
+          'Plumber',
+          'School Counselor',
+          'Real Estate Agent',
+          'Recreation & Fitness Worker',
+          'Writer',
+          'Landscaper & Groundskeeper',
+          'Cashier',
+          'Registered Nurse'
+      ],
+      Office: [
+          'New York',
+          'Edinburgh',
+          'Edinburgh',
+          'London',
+          'New York',
+          'London',
+          'London',
+          'Edinburgh',
+          'San Francisco',
+          'San Francisco',
+          'Henderson',
+          'Philadelphia',
+          'Miami',
+          'Nashville-Davidson',
+          'Miami',
+          'St. Paul',
+          'Arlington',
+          'San Francisco',
+          'Sacramento',
+          'Arlington',
+          'Stockton',
+          'Baton Rouge',
+          'Stockton',
+          'Hialeah',
+          'Las Vegas',
+          'Boise',
+          'Lexington-Fayette',
+          'Newark',
+          'Greensboro',
+          'Durham',
+          'Henderson',
+          'Indianapolis',
+          'Chesapeake',
+          'Mesa',
+          'Glendale',
+          'El Paso',
+          'Akron',
+          'Raleigh',
+          'Chandler',
+          'Birmingham',
+          'Rochester',
+          'Miami',
+          'Dallas',
+          'Raleigh',
+          'Toledo',
+          'Philadelphia',
+          'Columbus',
+          'Memphis',
+          'Philadelphia',
+          'Aurora',
+          'Fort Worth',
+          'Tulsa',
+          'Miami',
+          'Arlington',
+          'North Hempstead',
+          'Tucson',
+          'Riverside',
+          'Cleveland',
+          'St. Paul',
+          'San Diego'
+      ],
+      Age: [
+          '61',
+          '22',
+          '43',
+          '43',
+          '61',
+          '66',
+          '41',
+          '35',
+          '59',
+          '56',
+          '60',
+          '71', 
+          '29',
+          '46',
+          '36',
+          '55',
+          '24',
+          '57',
+          '73',
+          '70',
+          '45',
+          '58',
+          '51',
+          '66',
+          '49',
+          '43',
+          '19',
+          '47',
+          '57',
+          '68',
+          '48',
+          '74',
+          '39',
+          '20',
+          '71',
+          '56',
+          '73',
+          '64',
+          '59',
+          '20',
+          '37',
+          '66',
+          '23',
+          '36',
+          '41',
+          '39',
+          '56',
+          '27',
+          '63',
+          '52',
+          '62',
+          '63',
+          '52',
+          '23',
+          '44',
+          '57',
+          '65',
+          '25',
+          '31',
+          '59'
+      ],  
+      'Start date': [
+          '2013/08/11',
+          '2013/03/03',
+          '2013/02/01',
+          '2012/12/18',
+          '2012/12/02',
+          '2012/11/27',
+          '2012/10/13',
+          '2012/09/26',
+          '2012/08/06',
+          '2012/06/01',
+          '2010/05/18',
+          '2010/07/16',
+          '2011/02/21',
+          '2011/09/26',
+          '2011/10/20',
+          '2011/12/30',
+          '2012/02/09',
+          '2012/06/25',
+          '2012/11/20',
+          '2013/05/22',
+          '2013/07/05',
+          '2013/09/04',
+          '2014/09/23',
+          '2014/10/27',
+          '2014/11/24',
+          '2015/01/01',
+          '2015/05/29',
+          '2015/11/05',
+          '2017/01/16',
+          '2017/03/07',
+          '2017/08/02',
+          '2017/10/13',
+          '2017/10/17',
+          '2017/12/01',
+          '2018/05/23',
+          '2010/05/05',
+          '2010/07/02',
+          '2011/08/18',
+          '2012/08/03',
+          '2012/08/06',
+          '2012/11/01',
+          '2013/05/08',
+          '2014/01/13',
+          '2014/01/20',
+          '2014/07/22',
+          '2015/01/05',
+          '2015/01/16',
+          '2015/01/21',
+          '2015/04/30',
+          '2015/06/24',
+          '2015/07/24',
+          '2015/12/02',
+          '2016/01/18',
+          '2016/05/03',
+          '2016/07/06',
+          '2016/08/29',
+          '2018/04/23',
+          '2018/05/29',
+          '2018/06/04',
+          '2018/10/22'
+      ],
+      Salary: [
+          '$98,540',
+          '$342,000',
+          '$75,650',
+          '$313,500',
+          '$372,000',
+          '$198,500',
+          '$132,000',
+          '$217,500',
+          '$137,500',
+          '$115,000',
+          '$81,885',
+          '$193,579',
+          '$33,085',
+          '$162,766',
+          '$42,297',
+          '$150,871',
+          '$149,242',
+          '$62,476',
+          '$44,135',
+          '$131,589',
+          '$79,462',
+          '$97,843',
+          '$180,308',
+          '$45,046',
+          '$95,563',
+          '$145,280',
+          '$132,637',
+          '$191,496',
+          '$93,986',
+          '$191,991',
+          '$186,509',
+          '$50,458',
+          '$194,804',
+          '$69,384',
+          '$177,576',
+          '$36,338',
+          '$122,377',
+          '$104,770',
+          '$197,493',
+          '$39,568',
+          '$131,533',
+          '$79,377',
+          '$33,147', 
+          '$134,619',
+          '$42,142',
+          '$45,444',
+          '$85,490',
+          '$161,385',
+          '$77,921',
+          '$99,625',
+          '$92,974',
+          '$127,384',
+          '$167,468',
+          '$73,281',
+          '$142,431',
+          '$63,031',
+          '$188,562',
+          '$75,067',
+          '$54,526',
+          '$82,548'
+      ]
+  };
+
+var tableHolder = document.createElement('div');
+tableHolder.classList.add('clearfix');
+document.body.appendChild(tableHolder);
+
+var table = document.createElement('table');
+var tbody = document.createElement('tbody');
+var searchPanel = document.createElement('div');
+var searchInput = document.createElement('input');
+var searchButton = document.createElement('input');
+var trh = document.createElement('tr');
+var tr;
+var currentPage = 1;
+var navPanel = document.createElement('div');
+var previous = document.createElement('div');
+var next = document.createElement('div');
+var numberOfPages;
+
+previous.classList.add('inactiveDiv');
+next.classList.add('activeDiv');
+
+searchButton.type = 'button';
+searchButton.value = 'search';
+searchButton.classList.add('search');
+
+searchInput.style.type = 'text';
+
+searchInput.classList.add('navigation-panel');
+searchPanel.appendChild(searchButton);
+searchPanel.appendChild(searchInput);
+tableHolder.appendChild(searchPanel);
+
+searchPanel.classList.add('navigation-panel');
+
+for(var key in content) {
+
+  var th = document.createElement('th');
+  th.textContent = key;
+  trh.appendChild(th);
+
+}
+tbody.appendChild(trh);
+
+for(var i in content) {
+
+	var contentNumber = content[i].length;
+  break;
+  
+}
+
+for(var row = 0; row < contentNumber ; row++) {
+
+  tr = document.createElement('tr');
+  for(key in content) {
+
+    var td = document.createElement('td');
+    td.textContent = content[key][row] || '';
+    tr.appendChild(td);
+
+  }
+
+  tbody.appendChild(tr);
+
+}
+
+table.appendChild(tbody);
+
+buttonPrep(currentPage);
+tableHolder.appendChild(table);
+
+navPanel.classList.add('navigation-panel');
+tableHolder.appendChild(navPanel);
+
+previous.textContent = 'Previous';
+next.textContent = 'Next';
+
+updateButton(contentNumber);
+
+tableHolder.onclick = function(e) {
+
+  var target = e.target;
+
+  if(target == e.target.closest('th')) {
+
+    Sorting(target.textContent, target.cellIndex);
+
+	}else if(target == e.target.closest('.pageList')) {
+
+    var bs = navPanel.getElementsByTagName('button');
+
+    for(var i = 0; i < bs.length; i++) {
+
+      if(bs[i].textContent == currentPage) {
+
+        bs[i].classList.add('inactiveButton');
+
+      }
+
+    }
+
+    target.classList.remove('inactiveButton');
+    currentPage = target.value;
+
+    checkPage();
+
+    buttonPrep(currentPage);
+
+  }else if(target == e.target.closest('.search')) {
+
+    filterTable(searchInput.value);
+
+  }else if(target == e.target.closest('.activeDiv')) {
+
+    bs = navPanel.getElementsByTagName('button');
+
+    for(i = 0; i < bs.length; i++) {
+
+      if(bs[i].textContent == currentPage) {
+
+        bs[i].classList.add('inactiveButton');
+        if(target == next) {
+          bs[i - 1].classList.remove('inactiveButton');
+          currentPage++;
+        }else if(target == previous) {
+          bs[i + 1].classList.remove('inactiveButton');
+          currentPage--;
+          break;
+        } 
+      }
+    }
+    checkPage();
+    buttonPrep(currentPage);
+  }
+
+  function filterTable(keyWord) {
+
+    var tempbody = table.getElementsByTagName('tbody')[0];
+    var rowsArray = [].slice.call(tbody.rows, 1);
+    var newArray = rowsArray.filter(function(row) {
+
+      for(var i = 0; i < row.cells.length; i++) {
+
+        if(row.cells[i].textContent == keyWord) {
+
+          return row;
+
+        }
+
+      }
+
+    });
+    if(keyWord == '') {
+
+      table.removeChild(tempbody);
+      table.appendChild(tbody);
+      updateButton(rowsArray.length);
+
+    }
+    if(newArray.length == 0) {
+
+      return;
+
+    }
+    updateTable(tempbody, newArray, 1);
+
+  }
+
+  function checkPage() {
+    if(currentPage == 6) {
+      next.classList.remove('activeDiv');
+      next.classList.add('inactiveDiv');
+      previous.classList.add('activeDiv');
+      previous.classList.remove('inactiveDiv');
+    }else if(currentPage == 1) {
+      previous.classList.remove('activeDiv');
+      previous.classList.add('inactiveDiv');
+      next.classList.add('activeDiv');
+      next.classList.remove('inactiveDiv');
+    }else{
+      next.classList.add('activeDiv');
+      next.classList.remove('inactiveDiv');
+      previous.classList.add('activeDiv');
+      previous.classList.remove('inactiveDiv');
+    }
+
+  }
+
+  function Sorting(name, number) {
+
+    var tbody = table.getElementsByTagName('tbody')[0];
+    var rowsArray = [].slice.call(tbody.rows, 1);
+    var compare;
+
+    if( (!target.classList.contains('upSort') && !target.classList.contains('downSort')) || 
+          target.classList.contains('downSort') ) {
+
+      target.classList.toggle('upSort');
+      target.classList.remove('downSort');
+
+    }else if(target.classList.contains('upSort')) {
+
+      target.classList.remove('upSort');
+      target.classList.add('downSort');
+
+    }
+    
+    compare = function(a, b) {
+
+        a = a.cells[number].textContent;
+        b = b.cells[number].textContent;
+        var reg;
+        switch (name) {
+            
+            case 'Start date':
+                reg = /(\d+)\/(\d+)\/(\d+)/;
+                a = +a.match(reg).splice(1, 3).join('');
+                b = +b.match(reg).splice(1, 3).join('');
+                break;
+            case 'Age':
+                a = +a;
+                b = +b;
+                break;
+            case 'Salary':
+                reg = /\$(\d+),(\d+)/;
+                a = +a.match(reg).splice(1, 2).join('');
+                b = +b.match(reg).splice(1, 2).join('');
+            break;
+
+        } 
+        if(target.classList.contains('downSort')) {
+
+          var temp = a;
+          a = b;
+          b = temp;
+
+        }
+        if(a < b) {
+
+            return -1;
+
+        }else if(a > b) {
+
+            return 1;
+
+        }
+        return 0;
+
+    };
+
+    rowsArray.sort(compare);
+    table.removeChild(tbody);
+
+    for (var i = 0; i < rowsArray.length; i++) {
+
+      tbody.appendChild(rowsArray[i]);
+
+    }
+    table.appendChild(tbody);
+    buttonPrep(currentPage);
+    
+  }
+
+};
+
+function updateTable(oldBody, newArray, startPage) {
+
+  table.removeChild(oldBody);
+  var newBody = document.createElement('tbody');
+  newBody.appendChild(trh.cloneNode(true));
+  for (var i = 0; i < newArray.length; i++) {
+
+    newBody.appendChild(newArray[i].cloneNode(true));
+
+  }
+  table.appendChild(newBody);
+  updateButton(newArray.length);
+  buttonPrep(startPage);
+
+}
+
+function updateButton(rows) {
+
+  var buttons = navPanel.getElementsByTagName('button');
+  for(var i = 0; i < buttons.length; i++) {
+
+    navPanel.removeChild(buttons[i--]);
+
+  }
+  navPanel.appendChild(next);
+  numberOfPages = Math.ceil( rows / 10 );
+  for(i = numberOfPages; i > 0; i--) {
+
+    var button = document.createElement('button');
+    button.value = button.textContent = i;
+    button.classList.add('pageList');
+    button.classList.add('inactiveButton');
+    if(button.textContent == currentPage) {
+
+      button.classList.remove('inactiveButton');
+
+    }
+    navPanel.appendChild(button);
+    
+  }
+  navPanel.appendChild(previous);
+
+}
+
+function buttonPrep(currentPage) {
+
+  var multiplier = currentPage * 10;
+  var counter = 0;
+  var arr = [];
+  for(var i = multiplier - 10; i < multiplier; i++) {
+
+    arr[counter++] = i;
+
+  }
+  showTable(arr);
+  
+}
+
+function showTable(pageNumber) {
+
+	var tbody = table.getElementsByTagName('tbody')[0];
+  var rowsArray = [].slice.call(tbody.rows, 1);
+  var end = pageNumber[pageNumber.length - 1];
+  var start = pageNumber[0]; 
+  var  counter = 0;
+	
+	for(var i = 0; i < start; i++) {
+
+    rowsArray[i].classList.add('invis');
+    
+  }
+  for(; i <= end; i++) {
+
+    if(rowsArray[i].rowIndex - 1 == pageNumber[counter]) {
+
+      rowsArray[i].classList.remove('invis');
+      counter++;
+
+    }else{
+
+      rowsArray[i].classList.add('invis');
+
+    }
+
+  }
+  for(; i < rowsArray.length; i++) {
+
+    rowsArray[i].classList.add('invis');
+    
+  }
+
+}
